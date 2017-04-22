39ce90e5b6c5059d9281887cb1e44a9e53fa2197
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-04-22 12:34:56 +0300

    chore: add firebase support

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyCQqAA-fTARrZPBeB76B1kxfiK2U3DfU44',
+  authDomain: 'debts-b18a1.firebaseapp.com',
+  databaseURL: 'https://debts-b18a1.firebaseio.com',
+  projectId: 'debts-b18a1',
+  storageBucket: 'debts-b18a1.appspot.com',
+  messagingSenderId: '337540422151'
+};
